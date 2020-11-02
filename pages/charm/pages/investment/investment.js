3da789c00d7f1295210d2d9c6a8278a9575f0c2b
YanDong <yd@sucstep.com>
2020-11-02 17:24:57 +0800

    2
    2

---
+++ pages/charm/pages/investment/investment.js
@@ -0,0 +1,73 @@
+// pages/charm/pages/historyCulture/historyCulture.js
+const app = getApp()
+const util = require('../../../../utils/util.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabName: ["招商宣传", "优惠政策", "优质项目"],
+    tabBar:0,
+  },
+  selectTab: function (e) {
+    this.setData({
+      tabBar:e.detail.TabCur
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
