fd2f9cf9766d88a6548c1c63c6be72e793ca7d7e
泽龙 <785306433@qq.com>
2020-11-03 17:54:00 +0800

    家政服务页面及底边栏组件警告修正
    底边栏组件wx:key未传值会引发警告，已修正

--- pages/life/pages/housekeeping/housekeeping.js
+++ pages/life/pages/housekeeping/housekeeping.js
@@ -1,66 +1,126 @@
-// pages/life/pages/housekeeping/housekeeping.js
+const util = require('../../../../utils/util.js')
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    bannerSrc: '../../../../image/jiazheng-banner.png',
+    contentData: [{
+      title: '搬家装修',
+      id: '01',
+      block: [{
+        img: '../../../../image/zwt.png',
+        blockId: '0101',
+        title: '爱宝贝亲子班',
+        distence: '3.2公里',
+        tips: ['食宿','双语']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0102',
+        title: '双语国际亲子班',
+        distence: '3.2公里',
+        tips: ['双语']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0103',
+        title: '双语国际亲子班',
+        distence: '320米',
+        tips: ['双语']
+      }]
+    }, {
+      title: '保姆月嫂',
+      id: '02',
+      block: [{
+        img: '../../../../image/zwt.png',
+        blockId: '0201',
+        title: '保姆',
+        distence: '3.2公里',
+        tips: ['服务周到','贴心']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0202',
+        title: '月嫂',
+        distence: '3.2公里',
+        tips: ['悉心','经验丰富','熟练']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0203',
+        title: '护理',
+        distence: '320米',
+        tips: ['标签1','标签2']
+      }]
+    }, {
+      title: '保洁疏通',
+      id: '03',
+      block: [{
+        img: '../../../../image/zwt.png',
+        blockId: '0301',
+        title: '爱宝贝亲子班',
+        distence: '3.2公里',
+        tips: ['食宿','双语']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0302',
+        title: '双语国际亲子班',
+        distence: '3.2公里',
+        tips: ['双语']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0303',
+        title: '双语国际亲子班',
+        distence: '320米',
+        tips: ['双语']
+      }]
+    }, {
+      title: '家电维修',
+      id: '04',
+      block: [{
+        img: '../../../../image/zwt.png',
+        blockId: '0401',
+        title: '爱宝贝亲子班',
+        distence: '3.2公里',
+        tips: ['食宿','双语']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0402',
+        title: '双语国际亲子班',
+        distence: '3.2公里',
+        tips: ['双语']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0403',
+        title: '双语国际亲子班',
+        distence: '320米',
+        tips: ['双语']
+      }]
+    }, {
+      title: '开锁换锁',
+      id: '05',
+      block: [{
+        img: '../../../../image/zwt.png',
+        blockId: '0501',
+        title: '爱宝贝亲子班',
+        distence: '3.2公里',
+        tips: ['食宿','双语']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0502',
+        title: '双语国际亲子班',
+        distence: '3.2公里',
+        tips: ['双语']
+      }, {
+        img: '../../../../image/zwt.png',
+        blockId: '0503',
+        title: '双语国际亲子班',
+        distence: '320米',
+        tips: ['双语']
+      }]
+    }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  getMore: function (e) {
+    util.pageJumpTo('./details-list/details-list', 'id', e.currentTarget.dataset.id)
+  },
+  getDetails: function (e) {
+    util.pageJumpTo('./details/details', 'id', e.currentTarget.dataset.id)
+  },
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
 
   }
 })
