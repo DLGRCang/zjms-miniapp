4de1f8bca4eee1ffa7a0c595a5dfb67a888f4bda
严冬 <yd@sucstep.com>
2020-11-23 17:38:07 +0800

    个人中心
    11

--- pages/plugin/home/home.js
+++ pages/plugin/home/home.js
@@ -9,6 +9,38 @@ Page({
   data: {
 
   },
+  //个人信息
+  userinfo(){
+    util.pageJump('../../pages/userCenter/pages/userinfo/userinfo')
+  },
+  //我的订单
+  myOrder() {
+    util.pageJump('../../pages/userCenter/pages/order/order')
+  },
+  //我的咨询
+  myConsult() {
+    util.pageJump('../../pages/userCenter/pages/consult/consult')
+  },
+  //我的预约
+  myAppointment() {
+    util.pageJump('../../pages/userCenter/pages/appointment/appointment')
+  },
+  //我的申请
+  myApply() {
+    util.pageJump('../../pages/userCenter/pages/apply/apply')
+  },
+  //我的卡券
+  myCard() {
+    util.pageJump('../../pages/userCenter/pages/card/card')
+  },
+  //我的参与
+  myJoin() {
+    util.pageJump('../../pages/userCenter/pages/join/join')
+  },
+  //电话本
+  myTel() {
+    util.pageJump('../../pages/userCenter/pages/tel/tel')
+  },
 
   /**
    * 生命周期函数--监听页面加载
