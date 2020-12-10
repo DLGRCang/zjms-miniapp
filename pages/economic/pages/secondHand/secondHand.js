07c85b99af3e31bd7c03ae335c0904134b11d511
Xuer <798435242@qq.co>
2020-12-10 16:31:08 +0800

    1

---
+++ pages/economic/pages/secondHand/secondHand.js
@@ -0,0 +1,96 @@
+// pages/economic/pages/secondHand/secondHand.js
+const app = getApp()
+const util = require('../../../../utils/util.js')
+const data = require('../../../../utils/data.js')
+Page({
+
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		imgUrl:app.globalData.imgUrl,
+		infotypeid: 'c84fffd5-dcc4-4d63-a770-0f5669a155a2',
+		infotypeid1: '7e517f20-9d2c-461c-8cc2-e0feeb5dd1f7',
+    page:1,
+		dataList: [], //二手车
+		dataList1: [], //二手房
+		
+
+	},
+
+	secondHandInfo: function () {
+		util.pageJump('../../../../pages/economic/pages/secondHandInfo/secondHandInfo')
+	},
+
+
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	getDataList: function () {
+		//加载数据列表
+		data.getArtelData(this.data.infotypeid, this.data.page).then(dataList => {
+			this.setData({
+        dataList: this.data.dataList.concat(dataList),
+      })
+			console.log(this.data.dataList);
+		})
+		data.getArtelData(this.data.infotypeid1, this.data.page).then(dataList => {
+			this.setData({
+        dataList1: this.data.dataList1.concat(dataList),
+      })
+			console.log(this.data.dataList1);
+		})
+	},
+	onLoad: function (options) {
+		this.getDataList()
+	},
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
+
+	},
+
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
+
+	},
+
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {
+
+	},
+
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {
+
+	},
+
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {
+
+	}
+})
