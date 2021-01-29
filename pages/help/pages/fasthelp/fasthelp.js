3ef1173ecf749dd259d7ca3171a76e1c452f2617
YanDong <1035945627@qq.com>
2021-01-29 09:58:55 +0800

    1
    1

--- pages/help/pages/fasthelp/fasthelp.js
+++ pages/help/pages/fasthelp/fasthelp.js
@@ -1,13 +1,27 @@
 // pages/help/pages/fasthelp/fasthelp.js
+const app = getApp()
+const util = require('../../../../utils/util.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    tab:["妇联求助", "公安求助", "法院求助", "调解求助"],
+    TabCur: 0,
+    scrollLeft:0
   },
 
+  tabSelect(e) {
+    console.log(e.currentTarget.dataset.id)
+    this.setData({
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id-1)*60
+    })
+  },
+  goTel(e){
+    util.callPhone(e.currentTarget.dataset.tel)
+  },
   /**
    * 生命周期函数--监听页面加载
    */
