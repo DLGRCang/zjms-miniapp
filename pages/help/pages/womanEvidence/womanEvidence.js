e3a20f9e61bac7499be4ad35d7fd0f77f5d10920
Xuer <798435242@qq.co>
2021-01-29 15:38:16 +0800

    11

--- pages/help/pages/womanEvidence/womanEvidence.js
+++ pages/help/pages/womanEvidence/womanEvidence.js
@@ -5,9 +5,92 @@ Page({
 	 * 页面的初始数据
 	 */
 	data: {
-
+		evidenceList: ['医疗证明', '残疾证明', '贫困证明'],
+		evidenceName: '',
+		imgList: [],
+		videoList: [],
+		src: ''
 	},
-
+	typeChange(e) {
+		this.setData({
+			evidenceName: this.data.evidenceList[e.detail.value],
+		})
+	},
+	//选择视频
+	ChooseVideo() {
+		wx.chooseVideo({
+			success: (res) => {
+				this.setData({
+					src: res.tempFilePath
+				})
+				console.log(res.tempFilePath)
+				if (this.data.videoList.length != 0) {
+					let paths = this.data.videoList;
+					paths.push(res.tempFilePath)
+					this.setData({
+						videoList: paths
+					})
+				} else {
+					this.setData({
+						videoList: [res.tempFilePath]
+					})
+				}
+			}
+		});
+	},
+	//选择图片
+	ChooseImage() {
+		wx.chooseImage({
+			count: 1, //默认9
+			sizeType: ['original', 'compressed'],
+			sourceType: ['album'],
+			success: (res) => {
+				console.log(res.tempFilePaths[0]);
+				if (this.data.imgList.length != 0) {
+					this.setData({
+						imgList: this.data.imgList.concat(res.tempFilePaths)
+					})
+				} else {
+					this.setData({
+						imgList: res.tempFilePaths
+					})
+				}
+			}
+		});
+	},
+	//视频预览/图片预览
+	ViewVideo(e) {
+		wx.previewMedia({
+			sources: [{
+				url: e.currentTarget.dataset.url,
+				type:  e.currentTarget.dataset.type
+			}],
+		});
+	},
+	//删除图片/视频
+	DelImg(e) {
+		wx.showModal({
+			content: '确定要删除吗？',
+			cancelText: '再想想',
+			confirmText: '确定',
+			success: res => {
+				if (res.confirm) {
+					if(e.currentTarget.dataset.type=='video'){
+						this.data.videoList.splice(e.currentTarget.dataset.index, 1);
+						this.setData({
+							videoList: this.data.videoList
+						})
+					}else{
+						this.data.imgList.splice(e.currentTarget.dataset.index, 1);
+						this.setData({
+							imgList: this.data.imgList
+						})
+					}
+				
+				}
+			}
+		})
+	},
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
