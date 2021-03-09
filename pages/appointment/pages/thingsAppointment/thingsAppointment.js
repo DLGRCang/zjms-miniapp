189468dff21e956a53e1483c11902af4880ed3c2
YanDong <1035945627@qq.com>
2021-03-09 11:18:21 +0800

    1
    1

--- pages/appointment/pages/thingsAppointment/thingsAppointment.js
+++ pages/appointment/pages/thingsAppointment/thingsAppointment.js
@@ -1,18 +1,43 @@
 // pages/appointment/pages/thingsAppointment/thingsAppointment.js
+const app = getApp()
+const util = require('../../../../utils/util')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    waitInfo: null
   },
-
+  // 获取大厅排队信息
+  getInfo() {
+    let url = 'https://yiqi.sucstep.com/InfoIssue/app/release/queue/getStatus';
+    util.httpRequest(url, 'GET', {}).then(res => {
+      console.log(res.data)
+      if (res.data.is_mock === 1) {
+        wx.showToast({
+          title: '服务器开小差了',
+          icon: 'none',
+          success(res) {
+            setTimeout(() => {
+              wx.navigateBack({
+                delta: 1
+              })
+            }, 3000)
+          }
+        })
+      } else {
+        this.setData({
+          waitInfo: res.data
+        })
+      }
+    });
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getInfo()
   },
 
   /**
