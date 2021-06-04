26b14a952f9a0399f549e34b0fb1a5f352852dba
YanDong <1035945627@qq.com>
2021-06-04 10:38:45 +0800

    11
    1111

--- pages/appointment/pages/carInfo/carInfo.js
+++ pages/appointment/pages/carInfo/carInfo.js
@@ -1,18 +1,39 @@
 // pages/appointment/pages/carInfo/carInfo.js
+const app = getApp()
+const util = require('../../../../utils/util.js')
+var WxParse = require('../../../../wxParse/wxParse.js');
+var WxParse = require('../../../../wxParse/wxParse.js');
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    id: '',
+    info: null,
+  },
+  getInfo() {
 
+    let url = 'https://www.yjhlcity.com/InfoIssue/app/release/infocontent/getinfocontent/' + this.data.id
+    console.log(url)
+    util.httpRequest(url, 'GET', {}).then(res => {
+      console.log(res.data)
+      WxParse.wxParse('dataHtml', 'html', res.data.info_detail, this, 5)
+      this.setData({
+        info:res.data,
+      })
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    console.log(options)
+    this.setData({
+      id: options.infoContentId
+    })
+    this.getInfo()
   },
 
   /**
