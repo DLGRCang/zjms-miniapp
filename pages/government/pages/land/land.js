c03da0f34cfcff627e7c613e76ef1135200c4803
YanDong <1035945627@qq.com>
2021-06-15 09:06:21 +0800

    1111
    1111

--- pages/government/pages/land/land.js
+++ pages/government/pages/land/land.js
@@ -1,6 +1,7 @@
 // pages/government/pages/land/land.js
 const app = getApp()
 const util = require('../../../../utils/util.js')
+const dataUtil = require('../../../../utils/data.js')
 Page({
 
 	/**
@@ -8,21 +9,31 @@ Page({
 	 */
 	data: {
 		imgUrl: app.globalData.imgUrl,
+		id:'f1947c89-b473-4350-abef-e1370a21889d',
 		listInfo: [],
 		tit:''
 	},
 	getInfo() {
-		let that = this;
-		util.requestApi('landadoption/listlandadoption', 'GET', {}).then(res => {
-			console.log(res.data)
-			that.setData({
-				listInfo: res.data
+		// let that = this;
+		// util.requestApi('landadoption/listlandadoption', 'GET', {}).then(res => {
+		// 	console.log(res.data)
+		// 	that.setData({
+		// 		listInfo: res.data
+		// 	})
+		// })
+		dataUtil.getArtelData('f1947c89-b473-4350-abef-e1370a21889d', this.data.page, 50).then(dataList => {
+			console.log(dataList)
+			this.setData({
+				listInfo: this.data.listInfo.concat(dataList)
 			})
 		})
 	},
 	getDetail(e) {
-		console.log(e.currentTarget.dataset.id)
-		util.pageJumpTo("/pages/government/pages/landDetail/landDetail","id",e.currentTarget.dataset.id)
+		// console.log(e.currentTarget.dataset.id)
+		// util.pageJumpTo("/pages/government/pages/landDetail/landDetail","id",e.currentTarget.dataset.id)
+		wx.navigateTo({
+			url: "/pages/appointment/pages/carInfo/carInfo?infoContentId=" + e.currentTarget.dataset.id,
+		})
 	},
 	/**
 	 * 生命周期函数--监听页面加载
